36bd5d27d955185367c4caa63bc5f3df38afb80a
大碗臊子干拌面 <131854300+SJQj@users.noreply.github.com>
2024-10-25 11:14:50 +0800

    Update main.py

--- main/main.py
+++ main/main.py
@@ -229,7 +229,7 @@ if __name__ == "__main__":
         st.write('\n')
         st.write("你可以从侧面导航栏选择你想进行的操作")
         download_button("样例下载", r"main/static/样例.xlsx", 'xlsx')
-
+        uploaded_file = st.file_uploader("上传文件", type=["xlsx"])
     elif page == "CREATE":
         st.header("CREATE页面 ")
         sample_image = Image.open("main/image/create.png")
