408c5f92a9db8f691abf8b70d7f621827114c472
SJQ <shanjianqing2022@163.com>
2024-11-03 11:44:45 +0800

    优化

--- main/main.py
+++ main/main.py
@@ -45,11 +45,7 @@ def bulk_insert(path):
         df_dict['values'] = df.iloc[i, 2]
         df_list.append(df_dict)
     for info in df_list:
-        insert_statement = f"INSERT INTO {info.get('table')} (\n" + \
-                           "\n".join(f"    {column}" for column in info.get('column')) + \
-                           f"\n) VALUES (\n" + \
-                           "\n".join(f"    {value}" for value in info.get('values')) + \
-                           f"\n);"
+        insert_statement = f"INSERT INTO {info.get('table')} ({info.get('column')}) VALUES ({info.get('values')});"
         isrt_list.append(insert_statement)
     return isrt_list
 
