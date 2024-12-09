b688134127f59fd2ee379ad97ace90992cc4bd46
大碗臊子干拌面 <131854300+SJQj@users.noreply.github.com>
2024-12-09 10:52:05 +0800

    Update main.py

--- main/main.py
+++ main/main.py
@@ -382,7 +382,7 @@ if __name__ == "__main__":
         for k, v in dy_statements.items():
             dy_dict = {
                 "domain": domain,
-                "entity": k,
+                "entity": staging_{k},
                 "delimiter": ",",
                 "file_inzip_pattern": "",
                 "file_inzip_suffix": "",
